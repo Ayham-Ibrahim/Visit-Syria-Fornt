8550c0ea79fcb54f0e10f17d73e7ffa279cfb586
bushraYounes <boushra.ali.younes@gmail.com>
2024-07-10 20:12:07 +0300

    fix homepage v1

--- src/pages/HomePage/Homepage.jsx
+++ src/pages/HomePage/Homepage.jsx
@@ -17,68 +17,71 @@ function HomePage() {
   return (
     <div className='home-page'>
       <Hero backgroundImage={homeCover} />
-      <div className="public-preif-section">
-        <div className="row">
-          <div className="col-lg-7 col-md-12 order-lg-1 order-md-2 order-sm-2">
-            <div className="image-syria-map">
-              <div className="shape">
-                <img src={priefImage} alt="shape - rectangle" />
-              </div>
-              <div className="shape-responsive">
-                <img src={shapeImage} alt="shape - rectangle" />
-              </div>
-              <div className="syria-map">
-                <img src={syriaShImage} alt="" className="syria-sh" />
-                <img src={syriaImageLine} alt="map of syria" />
+      <div className="homePageContentContainer">
+        <div className="public-preif-section">
+          <div className="row">
+            <div className="col-lg-7 col-md-12 order-lg-1 order-md-2 order-sm-2">
+              <div className="image-syria-map">
+                <div className="shape">
+                  <img src={priefImage} alt="shape - rectangle" />
+                </div>
+                <div className="shape-responsive">
+                  <img src={shapeImage} alt="shape - rectangle" />
+                </div>
+                <div className="syria-map">
+                  <img src={syriaShImage} alt="" className="syria-sh" />
+                  <img src={syriaImageLine} alt="map of syria" />
+                </div>
               </div>
             </div>
-          </div>
-          <div className="col-lg-5 col-md-12 order-lg-2 order-md-1 order-sm-1">
-            <div className="brief-text-container">
-              <div className="brief-title">
-                <h1>نبذة عامة</h1>
+            <div className="col-lg-5 col-md-12 order-lg-2 order-md-1 order-sm-1">
+              <div className="brief-text-container">
+                <div className="brief-title">
+                  <h1>نبذة عامة</h1>
+                </div>
+                <div className="brief-text">
+                  <p>
+                    تقع سوريا في منطقة الشرق الأوسط، وتتمتع بموقع جغرافي متميز، حيث تربط بين آسيا وأفريقيا وأوروبا. هذا الموقع جعلها مركزاً تجارياً وثقافياً مهماً من آلاف السنين تمتلك سوريا تاريخًا عريقًا يعود إلى آلاف السنين، حيث كانت موطنًا للعديد من الحضارات القديمة، مثل الحضارة الآشورية والحضارة البابلية والحضارة الفينيقية
+                  </p>
+                </div>
+                <Button
+                  btnText="اقرأ المزيد"
+                  onClick={() => navigate('/about/history')} />
               </div>
-              <div className="brief-text">
-                <p>
-                  تقع سوريا في منطقة الشرق الأوسط، وتتمتع بموقع جغرافي متميز، حيث تربط بين آسيا وأفريقيا وأوروبا. هذا الموقع جعلها مركزاً تجارياً وثقافياً مهماً من آلاف السنين تمتلك سوريا تاريخًا عريقًا يعود إلى آلاف السنين، حيث كانت موطنًا للعديد من الحضارات القديمة، مثل الحضارة الآشورية والحضارة البابلية والحضارة الفينيقية
-                </p>
-              </div>
-              <Link to='/brief' className="brief-button pt-2">
-                <button>{"<"}  اقرأ المزيد </button>
-              </Link>
             </div>
           </div>
         </div>
-      </div>
-      <div className="home-blog-card mb-5">
-        <div className="home-blog-container">
-          <div className="card-blog-title_responsive">
-            <h1>المدونة </h1>
-          </div>
-          <div className="home-blog-content">
-            <div className="home-blog-image">
-              <div className="background-squre"></div>
+        <div className="home-blog-card mb-5">
+          <div className="home-blog-container">
+            <div className="card-blog-title_responsive">
+              <h1>المدونة </h1>
+            </div>
+            <div className="home-blog-content">
+              <div className="home-blog-image">
+                <div className="background-squre"></div>
+                <img src={blogCardImage} alt="blog card - ruins of Palmyra" />
+              </div>
               <img src={blogCardImage} alt="blog card - ruins of Palmyra" />
-            </div>
-            <img src={blogCardImage} alt="blog card - ruins of Palmyra" />
-            <div className="home-blog-text">
-              <div className="card-blog-title">
-                <h1>المدونة </h1>
+              <div className="home-blog-text">
+                <div className="card-blog-title">
+                  <h1>المدونة </h1>
+                </div>
+                <div className="card-blog-text">
+                  <p>
+                    اقرأ المدونات التي تتحدث عن طبيعة وآثار سوريا، وسوف تجد نفسك على رحلة استكشافية مثيرة في عالم غني بالتنوع البيئي و السياحي و الأثري والجمال الطبيعي
+                  </p>
+                </div>
+                <Button
+                  btnText="من هنا"
+                  className="BY_card_blog_button_home"
+                  onClick={() => navigate('/blogs/marks')} />
+
               </div>
-              <div className="card-blog-text">
-                <p>
-                  اقرأ المدونات التي تتحدث عن طبيعة وآثار سوريا، وسوف تجد نفسك على رحلة استكشافية مثيرة في عالم غني بالتنوع البيئي و السياحي و الأثري والجمال الطبيعي
-                </p>
-              </div>
-              <Button
-                btnText= "من هنا"
-                className="BY_card_blog_button_home" 
-                onClick={() => navigate('/blogs/marks')} />
-            
             </div>
           </div>
         </div>
       </div>
+
     </div>
   )
 }

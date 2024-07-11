7ae291e269bfadc4f968d0af0d3b45d152ae65ec
bushraYounes <boushra.ali.younes@gmail.com>
2024-07-11 10:36:50 +0300

    pagination

--- src/components/HomeRecommendationSection/HomeRecommendationSection.jsx
+++ src/components/HomeRecommendationSection/HomeRecommendationSection.jsx
@@ -1,9 +1,28 @@
 import React from 'react'
+import './HomeRecommendationSection.css'
+import Card from '../../shared/Card/Card'
+import { IoIosArrowBack } from "react-icons/io";
+import { IoIosArrowForward } from "react-icons/io";
+import { GoDotFill } from "react-icons/go";
 
 const HomeRecommendationSection = () => {
-  return (
-    <div>HomeRecommendationSection</div>
-  )
+    return (
+        <div className='BY_RecommendationSectionCardsContainer'>
+            HomeRecommendationSection
+            <div className='BY_CardsSection'>
+                {/* <Card/> */}
+            </div>
+            <div className='BY_PaginationSection'>
+                <div className="BY_leftArrow"><IoIosArrowBack /></div>
+                <div className="BY_dots">
+                    <div className="BY_dot"><GoDotFill /></div>
+                    <div className="BY_dot"><GoDotFill /></div>
+                    <div className="BY_dot"><GoDotFill /></div>
+                </div>
+                <div className="BY_rightArrow"><IoIosArrowForward /></div>
+            </div>
+        </div>
+    )
 }
 
 export default HomeRecommendationSection
